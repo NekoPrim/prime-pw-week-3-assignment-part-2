903b01089db5d9225434224da055c77128dbf8aa
Desiree Quade <nekodez@gmail.com>
2021-11-09 20:50:23 -0600

    1d stretch finished

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -14,7 +14,7 @@ for (let i=0; i<4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 
-for (let i=0; i<6; i++) {
+for (i=0; i < 6; i++) {
   console.log(i);
 }
 
@@ -22,7 +22,7 @@ for (let i=0; i<6; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
 
-for (let i=3; i<6; i++) {
+for (i=3; i < 6; i++) {
   console.log(i);
 }
 
@@ -30,14 +30,16 @@ for (let i=3; i<6; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
-for (let i=2; i<11; i += 2) {
+for (let i=2; i < 11; i += 2) {
   console.log(i);
 }
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
-
+for (let i=5; i >= 0; i --){
+  console.log(i);
+}
 
 // 2. For of loops
 console.log('---- 2. For Of loop ----');
@@ -56,17 +58,17 @@ console.log('---- 3. While loop ----');
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
-while (stars<stars.length){
+while (stars < stars.length){
   console.log(stars);
 }
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
-let i=0;
-const max=5;
+i=0;
+const max=6;
 
-while (i<max){
+while (i < max){
   console.log(i);
   i ++;
 }
@@ -75,9 +77,9 @@ while (i<max){
 console.log('count backwards from 10 to 5');
 
 i=10;
-const min=0;
+const min=4;
 
-while (i>min){
+while (i > min){
   console.log(i);
   i --;
 }
